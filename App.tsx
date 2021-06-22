5691b2c70eee1c2d76f0a3d2c2a1a83b6c6ee8d9
Henrique França <euhenriquefranca@gmail.com>
2021-06-21 22:05:36 -0300

    custom fonts, improvement signin and gradients.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,34 @@
 import React from "react";
+import { StatusBar } from "react-native";
+import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import AppLoading from "expo-app-loading";
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+
 import { SignIn } from "./src/screens/SignIn";
+import { Background } from "./src/components/Background";
 
 export default function App() {
-  return <SignIn />;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+  if (!fontsLoaded) {
+    <AppLoading />;
+  }
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <SignIn />
+    </Background>
+  );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, Image, StatusBar } from "react-native";
+import { View, Text, Image } from "react-native";
 
 import illustration from "../../assets/illustration.png";
 import { styles } from "./styles";
@@ -9,23 +9,17 @@ export function SignIn() {
   const [text, setText] = useState("");
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
       <Image source={illustration} style={styles.image} resizeMode="stretch" />
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize{`\n`}
-          suas jogatinas{`\n`}
-          facilmente
+          Conecte-se{`\n`}e organize suas{`\n`}
+          jogatinas
         </Text>
         <Text style={styles.subtitle}>
           Crie grupos para jogar seus games{`\n`}
           favoritos com seus amigos
         </Text>
-        <ButtonIcon title="Entrar com Discord" activeOpacity={0.7}/>
+        <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
       </View>
     </View>
   );
