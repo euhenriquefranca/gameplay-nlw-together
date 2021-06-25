43940ec8b71273adcd6e740f86c1b747cccdc7b0
Henrique França <euhenriquefranca@gmail.com>
2021-06-25 02:44:24 -0300

    server interface, scheduling, forms and modal

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 import { View, FlatList } from "react-native";
+
 import CategorySelect from "../../components/CategorySelect";
 import { ButtonAdd } from "../../components/ButtonAdd";
 import { Profile } from "../../components/Profile";
 import { ListHeader } from "../../components/ListHeader";
 import { Appointment } from "../../components/Appointment";
-import  ListDivider  from "../../components/ListDivider";
+import {ListDivider} from "../../components/ListDivider";
+import { Background } from "../../components/Background";
 
 import { styles } from "./styles";
 
 export function Home() {
   const [category, setCategory] = useState("");
+
+  const navigation = useNavigation();
+
   const appoinments = [
     {
       id: "1",
@@ -42,11 +48,19 @@ export function Home() {
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory("") : setCategory(categoryId);
   }
+
+  function handleAppointmentDetails() {
+    navigation.navigate("AppointmentDetails");
+  }
+  function handleAppointmentCreate() {
+    navigation.navigate("AppointmentCreate");
+  }
+
   return (
-    <View>
+    <Background>
       <View style={styles.header}>
         <Profile />
-        <ButtonAdd />
+        <ButtonAdd onPress={handleAppointmentCreate}/>
       </View>
       <View>
         <CategorySelect
@@ -58,13 +72,15 @@ export function Home() {
           <FlatList
             data={appoinments}
             keyExtractor={(item) => item.id}
-            renderItem={({ item }) => <Appointment data={item} />}
+            renderItem={({ item }) => (
+              <Appointment onPress={handleAppointmentDetails} data={item} />
+            )}
             ItemSeparatorComponent={() => <ListDivider />}
             style={styles.matches}
             showsVerticalScrollIndicator={false}
           />
         </View>
       </View>
-    </View>
+    </Background>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -5,6 +5,7 @@ import { View, Text, Image } from "react-native";
 import illustration from "../../assets/illustration.png";
 import { styles } from "./styles";
 import ButtonIcon from "../../components/ButtonIcon";
+import { Background } from "../../components/Background";
 
 export function SignIn() {
   const navigation = useNavigation();
@@ -13,22 +14,25 @@ export function SignIn() {
   }
   const [text, setText] = useState("");
   return (
-    <View style={styles.container}>
-      <Image source={illustration} style={styles.image} resizeMode="stretch" />
-      <View style={styles.content}>
-        <Text style={styles.title}>
-          Conecte-se{`\n`}e organize suas{`\n`}
-          jogatinas
-        </Text>
-        <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games{`\n`}
-          favoritos com seus amigos
-        </Text>
-        <ButtonIcon
-          title="Entrar com Discord"
-          onPress={handleSignIn}
+    <Background>
+      <View style={styles.container}>
+        <Image
+          source={illustration}
+          style={styles.image}
+          resizeMode="stretch"
         />
+        <View style={styles.content}>
+          <Text style={styles.title}>
+            Conecte-se{`\n`}e organize suas{`\n`}
+            jogatinas
+          </Text>
+          <Text style={styles.subtitle}>
+            Crie grupos para jogar seus games{`\n`}
+            favoritos com seus amigos
+          </Text>
+          <ButtonIcon title="Entrar com Discord" onPress={handleSignIn} />
+        </View>
       </View>
-    </View>
+    </Background>
   );
 }
